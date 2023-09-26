b49ea9d5b3248934676cca32d43203858b70d9e0
raazeshp96 <raazeshp96@gmail.com>
2023-09-26 16:53:38 +0545

    feat:addede manifest file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex items-center justify-center">
-          PWA app using next13
-        </div>
-      </div>
+      <div className="flex items-center justify-center">PWA with nextjs</div>
     </main>
   );
 }
