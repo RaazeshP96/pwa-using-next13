c612b33615e79ea995606e8778aa6d156788073d
raazeshp96 <raazeshp96@gmail.com>
2023-09-26 17:13:04 +0545

    feat:updateing metadata to include manifest file in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center justify-center">PWA with nextjs</div>
+      <div className="flex items-center justify-center text-8xl">
+        PWA with nextjs
+      </div>
     </main>
   );
 }
