93e90009463bcda236144101253f94f2e55fd35f
raazeshp96 <raazeshp96@gmail.com>
2023-09-26 17:25:49 +0545

    fix:configure next.config file for pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex items-center justify-center text-8xl">
         PWA with nextjs
       </div>
+      <Image
+        src={"/images/my-photo.jpg"}
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
     </main>
   );
 }
